85fd8ac931bbdad11cb859b43271e9327e885710
maneloy <maneloy5@gmail.com>
2017-08-11 20:00:12 -0300

    added "newton" from pset2

---
+++ MIT6_SC/pset2/ps2_newton.py
@@ -0,0 +1,76 @@
+# 6.00 Problem Set 2
+#
+# Successive Approximation
+#
+
+def evaluate_poly(poly, x):
+    """
+    Computes the polynomial function for a given value x. Returns that value.
+
+    Example:
+    >>> poly = (0.0, 0.0, 5.0, 9.3, 7.0)    # f(x) = 7x^4 + 9.3x^3 + 5x^2
+    >>> x = -13
+    >>> print evaluate_poly(poly, x)  # f(-13) = 7(-13)^4 + 9.3(-13)^3 + 5(-13)^2
+    180339.9
+
+    poly: tuple of numbers, length > 0
+    x: number
+    returns: float
+    """
+    result = 0
+    for coef in poly:
+        result += coef * (x**(poly.index(coef)))
+    return result
+
+
+def compute_deriv(poly):
+    """
+    Computes and returns the derivative of a polynomial function. If the
+    derivative is 0, returns (0.0,).
+
+    Example:
+    >>> poly = (-13.39, 0.0, 17.5, 3.0, 1.0)    # x^4 + 3x^3 + 17.5x^2 - 13.39
+    >>> print compute_deriv(poly)        # 4x^3 + 9x^2 + 35^x
+    (0.0, 35.0, 9.0, 4.0)
+
+    poly: tuple of numbers, length > 0
+    returns: tuple of numbers
+    """
+    if (len(poly) == 0):
+        return (0.0,)
+    derivative = tuple()
+    for coef in poly[1:]: # skip element of order 0, which will always cancel out.
+        new_coef = coef * poly.index(coef)        
+        derivative += (new_coef,)
+    return derivative
+
+def compute_root(poly, x_0, epsilon):
+    """
+    Uses Newton's method to find and return a root of a polynomial function.
+    Returns a tuple containing the root and the number of iterations required
+    to get to the root.
+
+    Example:
+    >>> poly = (-13.39, 0.0, 17.5, 3.0, 1.0)    #x^4 + 3x^3 + 17.5x^2 - 13.39
+    >>> x_0 = 0.1
+    >>> epsilon = .0001
+    >>> print compute_root(poly, x_0, epsilon)
+    (0.80679075379635201, 8.0)
+
+    poly: tuple of numbers, length > 1.
+         Represents a polynomial function containing at least one real root.
+         The derivative of this polynomial function at x_0 is not 0.
+    x_0: float
+    epsilon: float > 0
+    returns: tuple (float, int)
+    """
+    guess = x_0
+    iterations = 0
+    while True:
+        iterations += 1
+        ans = evaluate_poly(poly, guess)
+        if (abs(0 - ans) < epsilon):
+            return (guess, iterations)
+        else:
+            guess = guess - (evaluate_poly(poly, guess) / evaluate_poly(compute_deriv(poly), guess))
+

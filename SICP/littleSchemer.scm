221156dfb5078b80ed07c759687ba5c20ceb721a
maneloy <maneloy5@gmail.com>
2017-09-03 03:10:17 -0300

    Added solutions from 'The Little Schemer'

--- SICP/littleSchemer.scm
+++ SICP/littleSchemer.scm
@@ -12,26 +12,31 @@
 ;; Second Commandment: Use 'cons' to build lists.
 ;; Third Commandment: When building a list, describe the first typical element, and the cons it onto the natural recursion.
 
-(define atom? ;; strings of characters not enclosed by ()
+;; ~~~CODE~~~
+;; atom?: strings of characters not enclosed by ()
+(define atom?
   (lambda (x)
     (and (not (pair? x))
          (not (null? x)))))
 
-(define lat? ;; lists whose individual members are all atoms
+;; lat?: lists whose individual members are all atoms
+(define lat? 
   (lambda (l)
     (cond
       ((null? l) true)
       ((atom? (car l)) (lat? (cdr l)))
       (else false))))
 
-(define member? ;; is the atom 'a' a member of the list of atoms 'lat'?
+;; member?: is the atom 'a' a member of the list of atoms 'lat'?
+(define member? 
   (lambda (a lat)
     (cond
       ((null? lat) false)
       (else (or (eq? (car lat) a)
                 (member? a (cdr lat)))))))
 
-(define rember  ;; remove first ocurrence of 'a' in 'lat'.
+;; rember: remove first occurrence of 'a' in 'lat'.
+(define rember  
   (lambda (a lat)
     (cond
       ((null? lat) (quote ()))
@@ -39,16 +44,61 @@
       (else (cons (car lat)
                   (rember a (cdr lat)))))))
 
-(define firsts     ;; Takes a list of non-empty lists (or a null list) and 
-  (lambda (l)      ;; returns a list with the first S-exp of each list.
+;; firsts: Takes a list of non-empty lists (or a null list)
+;; and returns a list with the first S-exp of each list.
+(define firsts      
+  (lambda (l) 
     (cond
       ((null? l) (quote ()))
       (else (cons (car (car l))
                   (firsts (cdr l)))))))
 
-(define seconds    ;; Takes a list of non-empty lists (or a null list) and 
-  (lambda (l)      ;; returns a list with the second S-exp of each list.
+;; seconds: Takes a list of non-empty lists (or a null list) and
+;; returns a list with the second S-exp of each list.
+(define seconds    
+  (lambda (l)      
     (cond
       ((null? l) (quote ()))
       (else (cons (car (cdr (car l)))
                   (seconds (cdr l)))))))
+
+;; insertR: Inserts the atom 'new' to the right of the atom 'old' in the list 'lat'.
+(define insertR        
+  (lambda (new old lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? (car lat) old) (cons old (cons new (cdr lat))))
+          (else (cons (car lat)
+                      (insertR new old (cdr lat)))))))
+
+;; insertL: Inserts the atom 'new' to the right of the atom 'old' in the list 'lat'.
+(define insertL       
+  (lambda (new old lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? (car lat) old) (cons new lat))
+          (else (cons (car lat)
+                      (insertL new old (cdr lat)))))))
+
+;; subst: Replaces the atom 'old' for the atom 'new' in the list 'lat'.
+(define subst         
+  (lambda (new old lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? (car lat) old) (cons new (cdr lat)))
+          (else (cons (car lat)
+                      (subst new old (cdr lat)))))))
+
+;; subst2: Replaces either the first occurrence of 'o1' or that of 'o2' by 'new'.
+(define subst2        
+  (lambda (new o1 o2 lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((or (eq? (car lat) o1)
+           (eq? (car lat) o2)) (cons new (cdr lat)))
+      (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))
+
+;; multirember: removes all occurrences of 'a' from 'lat'
+(define multirember
+  (lambda (a lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((eq? (car lat) a) (multirember a (cdr lat)))
+      (else (cons (car lat) (multirember a (cdr lat)))))))
